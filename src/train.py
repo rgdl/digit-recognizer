5e6c15b0f1abd9cf67170f2044ad2f172f8375e9
rgdl <robert_delisle44@hotmail.com>
2022-07-01 01:58:36 +1000

    more methods and tests on trainer class

--- src/train.py
+++ src/train.py
@@ -10,8 +10,12 @@ from typing import Any
 from typing import Dict
 from typing import Type
 
+import pandas as pd  # type: ignore
 import pytorch_lightning as pl
+import torch
+import torch.nn.functional as F
 
+from consts import N_CLASSES
 from data_module import DataModule
 from models import ModelTools
 
@@ -34,3 +38,60 @@ class ModelTrainer:
 
     def fit(self) -> None:
         self.trainer.fit(self.model, datamodule=self.data)
+
+    def _get_dataloader(self, dataset: str) -> torch.utils.data.DataLoader:
+        dataset_dataloader_map = {
+            "train": self.data.train_dataloader(),
+            "valid": self.data.val_dataloader(),
+            "test": self.data.test_dataloader(),
+        }
+        try:
+            return dataset_dataloader_map[dataset]
+        except IndexError:
+            raise ValueError(
+                "`dataset` must be one of "
+                f"{list(dataset_dataloader_map.keys())}"
+            )
+
+    # TODO: Change to just predict test (hard-coded)
+    def batch_predict(self, dataset: str) -> torch.Tensor:
+        preds = []
+        with torch.no_grad():
+            for x, _ in self._get_dataloader(dataset):
+                preds.append(self.model(x).cpu().detach())
+        return torch.concat(preds)
+
+    def get_output_summary(self) -> pd.DataFrame:
+        probability_cols = [f"prob_{i}" for i in range(N_CLASSES)]
+
+        def _get_dataset_summary(dataset: str) -> pd.DataFrame:
+            preds, labels = [], []
+            with torch.no_grad():
+                for x, y in self._get_dataloader(dataset):
+                    preds.append(self.model(x).cpu().detach())
+                    labels.append(y.cpu().detach())
+            all_preds = F.softmax(torch.concat(preds), dim=1).numpy()
+            all_labels = torch.concat(labels).numpy()
+
+            result = pd.concat(
+                [
+                    pd.DataFrame(
+                        {
+                            "label": all_labels,
+                            "is_valid": dataset == "valid",
+                        }
+                    ),
+                    pd.DataFrame(all_preds, columns=probability_cols),
+                ],
+                axis=1,
+            )
+            result["pred"] = result.filter(regex="prob").values.argmax(axis=1)
+            result["correct"] = result["pred"] == result["label"]
+            return result
+
+        return pd.concat(
+            [
+                _get_dataset_summary("train"),
+                _get_dataset_summary("valid"),
+            ]
+        )
--- test/test_train.py
+++ test/test_train.py
@@ -2,14 +2,17 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 
 import pandas as pd  # type: ignore
+import pytest
 import pytorch_lightning as pl
 import torch
 
+from consts import N_CLASSES
 from models import BasicLinearModel
 from models import ModelTools
 from train import ModelTrainer
 
 
+# TODO: create a fixture for a model trainer excluding the case where we make a temp dir to store the logs, IF WE HAVE TO
 def test_loss_can_be_reduced():
     with TemporaryDirectory() as td:
         mt = ModelTrainer(
@@ -20,7 +23,7 @@ def test_loss_can_be_reduced():
                 loss_func=torch.nn.functional.cross_entropy,
             ),
             fold=0,
-            **{"max_epochs": 3, "logger": pl.loggers.CSVLogger(td)},
+            **{"max_epochs": 5, "logger": pl.loggers.CSVLogger(td)},
         )
 
         mt.fit()
@@ -29,3 +32,71 @@ def test_loss_can_be_reduced():
         first_val_loss = val_loss_logs.iloc[0]
         last_val_loss = val_loss_logs.iloc[-1]
         assert last_val_loss < first_val_loss
+
+
+datasets_and_dataloaders = [
+    ("train", "train_dataloader"),
+    ("valid", "val_dataloader"),
+    ("test", "test_dataloader"),
+]
+
+
+@pytest.mark.parametrize("dataset,dataloader", datasets_and_dataloaders)
+def test_batch_predict(dataset, dataloader):
+    mt = ModelTrainer(
+        BasicLinearModel,
+        ModelTools(
+            opt_class=torch.optim.SGD,
+            opt_args={"lr": 1e-3},
+            loss_func=torch.nn.functional.cross_entropy,
+        ),
+        fold=0,
+        **{"max_epochs": 3},
+    )
+    expected_shape = (len(getattr(mt.data, dataloader)().dataset), N_CLASSES)
+    assert mt.batch_predict(dataset).shape == expected_shape
+
+
+def test_get_output_summary():
+    mt = ModelTrainer(
+        BasicLinearModel,
+        ModelTools(
+            opt_class=torch.optim.SGD,
+            opt_args={"lr": 1e-3},
+            loss_func=torch.nn.functional.cross_entropy,
+        ),
+        fold=0,
+        **{"max_epochs": 3},
+    )
+    df = mt.get_output_summary()
+
+    assert df.notna().all(axis=None)
+
+    assert tuple(df.columns) == (
+        "label",
+        "is_valid",
+        *(f"prob_{i}" for i in range(N_CLASSES)),
+        "pred",
+        "correct",
+    )
+    assert len(df) == (
+        len(mt.data.train_dataloader().dataset)
+        + len(mt.data.val_dataloader().dataset)
+    )
+
+    assert df["label"].dtype == int
+    assert df["label"].min() == 0
+    assert df["label"].max() == 9
+
+    assert df["is_valid"].dtype == bool
+
+    for col in df.filter(regex="^prob_[0-9]$"):
+        assert df[col].dtype == "float32"
+        assert df[col].min() >= 0
+        assert df[col].max() <= 1
+
+    assert df["pred"].dtype == int
+    assert df["pred"].min() == 0
+    assert df["pred"].max() == 9
+
+    assert df["correct"].dtype == bool
